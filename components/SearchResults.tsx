c4256fc1e9aa75d7747cb358477dee328cbf0733
Ingrid Larissa <larissaorcx@gmail.com>
2022-03-11 23:55:16 -0300

    utilizando o useCallback

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -7,9 +7,10 @@ interface SearchResultsProps {
         price: number;
         title: string;
     }>
+    onAddToWishlist: (id: number) => void
 }
 
-export function SearchResults({results}: SearchResultsProps){
+export function SearchResults({results, onAddToWishlist}: SearchResultsProps){
     const totalPrice = useMemo(()=> {
         return results.reduce((total, product) => {
             return total + product.price;
@@ -21,7 +22,7 @@ export function SearchResults({results}: SearchResultsProps){
             <h2>Soma de todos os valores: {totalPrice}</h2>
             {results.map(product => {
                 return(
-                    <ProductItem key={product.id} product={product} />
+                    <ProductItem key={product.id} product={product} onAddToWishlist={onAddToWishlist}/>
                 );
             })}
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from "react"
+import { FormEvent, useCallback, useState } from "react"
 import { SearchResults } from "../components/SearchResults";
 
 
@@ -19,6 +19,10 @@ export default function Home() {
     setResults(data);
   }
 
+const addToWishlist = useCallback(async (id: number) => {
+  console.log(id)
+}, [])
+
   return (
     <div>
       <h1>Serch</h1>
@@ -34,7 +38,7 @@ export default function Home() {
           <button type="submit">Buscar</button>
       </form>
 
-      <SearchResults results={results} />
+      <SearchResults results={results}  onAddToWishlist={addToWishlist}/>
     </div>
   )
 }
