c4152ec88712c733820e275d4f71268789c81146
Ingrid Larissa <larissaorcx@gmail.com>
2022-03-11 21:25:39 -0300

    Inicio da aplicação

---
+++ pages/index.tsx
@@ -0,0 +1,40 @@
+import { FormEvent, useState } from "react"
+import { SearchResults } from "../components/SearchResults";
+
+
+export default function Home() {
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState([]);
+
+  async function handleaSerch(event: FormEvent) {
+    event.preventDefault()
+
+    if(!search.trim()){
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`)
+    const data = await response.json();
+
+    setResults(data);
+  }
+
+  return (
+    <div>
+      <h1>Serch</h1>
+
+      <form onSubmit={handleaSerch}>
+       <input
+          type="text"
+          value={search}
+          onChange={e => setSearch(e.target.value)} 
+          
+          />
+
+          <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  )
+}
